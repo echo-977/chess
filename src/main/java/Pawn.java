f4faf5aac4de632dd095f69141a998bf077e7162
echo-977 <sr1g24@soton.ac.uk>
2025-06-10 23:46:55 +0100

    Added Pawn piece and respective test file

---
+++ src/main/java/Pawn.java
@@ -0,0 +1,103 @@
+public class Pawn extends Piece{
+    private boolean moved;
+    private boolean enPassantable;
+
+    /**
+     * Constructs a pawn with the specified name, color, rank, and file.
+     * Ensures inputs for a piece are valid.
+     *
+     * @param colour         the colour of the piece ("White" or "Black")
+     * @param file           the file (column) position on the board in algebraic notation (e.g., "e")
+     * @param rank           the rank (row) position on the board in algebraic notation (e.g., "2")
+     * @param moved          whether the pawn has moved
+     * @param enPassantable  whether the pawn can be taken by en passant
+     */
+    public Pawn(String colour, char file, int rank, boolean moved, boolean enPassantable) {
+        super("Pawn", colour, file, rank);
+        this.moved = moved;
+        this.enPassantable = enPassantable;
+    }
+
+    /**
+     * Generates all the legal moves the queen can do (without considering other pieces).
+     * @return an array of all the squares the queen can move to as strings.
+     */
+    @Override
+    public String[] generateMoves() {
+        char file = getFile();
+        int checkRank = getRank();
+        String[] moves = new String[2]; //number of pawn moves in any position
+        String move;
+        int direction;
+        if (getColour().equals("White")) {
+            direction = 1;
+        } else {
+            direction = -1;
+        }
+        for (int i = 0; i < 2; i++) {
+            checkRank = checkRank + direction;
+            move = String.valueOf(file) + String.valueOf(checkRank);
+            if (isLegalMove(move)) {
+                moves[i] = move;
+            } else {
+                break;
+            }
+        }
+        return moves;
+    }
+    /**
+     * Check if a given move is legal (without considering other pieces).
+     * @param move the move to be validated.
+     * @return boolean value denoting if the move is theoretically legal.
+     */
+    @Override
+    public boolean isLegalMove(String move) {
+        if (!super.isLegalMove(move)) {
+            return false;
+        }
+        char file = getFile();
+        int rank = getRank();
+        char moveFile = move.charAt(0);
+        int moveRank = Integer.parseInt(move.substring(1, 2));
+        if (Math.abs(moveRank - rank) == 1 && moveFile == file) {
+            return true;
+        }
+        else {
+            return (!getMoved() && Math.abs(moveRank - rank) == 2 && moveFile == file);
+        }
+
+    }
+
+    /**
+     * Overrides the move method in the piece class to update the moved flag.
+     * @param move the square to move the piece to.
+     */
+    @Override
+    public void move(String move) {
+        if (!moved) {
+            moved = true;
+            enPassantable = true;
+        } else {
+            enPassantable = false;
+        }
+        super.move(move);
+    }
+
+    /**
+     * Simple getter for the boolean moved
+     * @return the moved boolean
+     */
+    public boolean getMoved() {
+        return moved;
+    }
+
+    /**
+     * Simple getter for the boolean enPassantable
+     * @return the enPassantable boolean
+     */
+    public boolean getEnPassantable() {
+        return enPassantable;
+    }
+}
+
+
---
+++ src/test/java/PawnTest.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class PawnTest {
+    public Pawn piece1;
+    public Pawn piece2;
+
+    @BeforeEach
+    public void init() {
+        piece1 = new Pawn("White", 'a', 1, false, false);
+        piece2 = new Pawn("Black", 'd', 5, false, false);
+    }
+
+    @Test
+    @DisplayName("Test isLegalMove")
+    void testLegalMove() {
+        assertTrue(piece1.isLegalMove("a2"));
+        assertTrue(piece1.isLegalMove("a3"));
+        assertTrue(piece2.isLegalMove("d4"));
+        assertTrue(piece2.isLegalMove("d3"));
+        assertFalse(piece1.isLegalMove("b1"));
+        assertFalse(piece2.isLegalMove("c5"));
+    }
+
+    @Test
+    @DisplayName("Test generateMoves")
+    void testGenerateMoves() {
+        String[] piece1MovesExpected = {"a2", "a3"};
+        String[] piece2MovesExpected = {"d4", "d3"};
+        String[] piece1MovesActual = piece1.generateMoves();
+        assertArrayEquals(piece1MovesExpected, piece1MovesActual);
+        String[] piece2MovesActual = piece2.generateMoves();
+        assertArrayEquals(piece2MovesExpected, piece2MovesActual);
+    }
+
+    @Test
+    @DisplayName("Test moved")
+    void testMoved() {
+        assertFalse(piece1.getMoved());
+        piece1.move("a2");
+        assertTrue(piece1.getMoved());
+    }
+
+    @Test
+    @DisplayName("Test enPassantable")
+    void testEnPassantable() {
+        assertFalse(piece1.getEnPassantable());
+        piece1.move("a3");
+        assertTrue(piece1.getEnPassantable());
+        piece1.move("a4");
+        assertFalse(piece1.getEnPassantable());
+    }
+}
