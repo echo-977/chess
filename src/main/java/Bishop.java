301ccf25854966adebaaeaed3bd3bbb250b78945
echo-977 <sr1g24@soton.ac.uk>
2025-06-10 20:26:19 +0100

    Added Bishop piece and test class for it

---
+++ src/main/java/Bishop.java
@@ -0,0 +1,98 @@
+public class Bishop extends Piece{
+
+    /**
+     * Constructs a bishop with the specified name, color, rank, and file.
+     * Ensures inputs for a piece are valid.
+     *
+     * @param colour the colour of the piece ("White" or "Black")
+     * @param file   the file (column) position on the board in algebraic notation (e.g., "e")
+     * @param rank   the rank (row) position on the board in algebraic notation (e.g., "2")
+     */
+    public Bishop(String colour, char file, int rank) {
+        super("Bishop", colour, file, rank);
+    }
+
+    /**
+     * Generates all the legal moves the bishop can do (without considering other pieces).
+     * @return an array of all the squares the rook can move to as strings.
+     */
+    @Override
+    public String[] generateMoves() {
+        char file = getFile();
+        int rank = getRank();
+        String[] moves = new String[13]; //max number of bishop moves in any position
+        int movesIndex = 0;
+        String move;
+        //bottom left diagonal
+        int numMoves = Math.abs(Math.min(Math.abs(rank - 1), Math.abs(file - 'a')));
+        char checkFile = (char) (file - numMoves);
+        int checkRank = rank - numMoves;
+        for (int i = 0; i < numMoves; i++) {
+            move = String.valueOf(checkFile) + String.valueOf(checkRank);
+            if (isLegalMove(move)) {
+                moves[movesIndex] = move;
+                movesIndex++;
+            }
+            checkFile = (char) (checkFile + 1);
+            checkRank = checkRank + 1;
+        }
+        //top right diagonal
+        numMoves = Math.abs(Math.min(Math.abs(rank - 8), Math.abs(file - 'h')));
+        checkFile = (char) (file + 1);
+        checkRank = rank + 1;
+        for (int i = 0; i < numMoves; i++) {
+            move = String.valueOf(checkFile) + String.valueOf(checkRank);
+            if (isLegalMove(move)) {
+                moves[movesIndex] = move;
+                movesIndex++;
+            }
+            checkFile = (char) (checkFile + 1);
+            checkRank = checkRank + 1;
+        }
+        //top left diagonal
+        numMoves = Math.abs(Math.min(Math.abs(rank - 8), file - 'a'));
+        checkFile = (char) (file - numMoves);
+        checkRank = rank + numMoves;
+        for (int i = 0; i < numMoves; i++) {
+            move = String.valueOf(checkFile) + String.valueOf(checkRank);
+            if (isLegalMove(move)) {
+                moves[movesIndex] = move;
+                movesIndex++;
+            }
+            checkFile = (char) (checkFile + 1);
+            checkRank = checkRank - 1;
+        }
+        //bottom right diagonal
+        numMoves = Math.abs(Math.min(rank - 1, Math.abs(file - 'h')));
+        checkFile = (char) (file + 1);
+        checkRank = rank - 1;
+        for (int i = 0; i < numMoves; i++) {
+            move = String.valueOf(checkFile) + String.valueOf(checkRank);
+            if (isLegalMove(move)) {
+                moves[movesIndex] = move;
+                movesIndex++;
+            }
+            checkFile = (char) (checkFile + 1);
+            checkRank = checkRank - 1;
+        }
+        return moves;
+    }
+
+    /**
+     * Check if a given move is legal (without considering other pieces).
+     * @param move the move to be validated.
+     * @return boolean value denoting if the move is theoretically legal.
+     */
+    @Override
+    public boolean isLegalMove(String move) {
+        if (!super.isLegalMove(move)) {
+            return false;
+        }
+        char moveFile = move.charAt(0);
+        int moveRank = Integer.parseInt(move.substring(1, 2));
+        char file = getFile();
+        int rank = getRank();
+        return Math.abs(rank - moveRank) == Math.abs(file - moveFile);
+
+    }
+}
---
+++ src/test/java/BishopTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class BishopTest {
+    static Bishop piece1;
+    static Bishop piece2;
+
+    @BeforeEach
+    public void init() {
+        piece1 = new Bishop("White", 'a', 1);
+        piece2 = new Bishop("Black", 'd', 5);
+    }
+
+    @Test
+    @DisplayName("Test isLegalMove")
+    void testLegalMove() {
+        assertTrue(piece1.isLegalMove("b2"));
+        assertTrue(piece2.isLegalMove("e4"));
+        assertFalse(piece1.isLegalMove("b6"));
+        assertFalse(piece2.isLegalMove("f8"));
+    }
+
+    @Test
+    @DisplayName("Test generateMoves")
+    void testGenerateMoves() {
+        String[] piece1MovesExpected = {"b2", "c3", "d4", "e5", "f6", "g7", "h8", null, null, null, null, null, null};
+        String[] piece2MovesExpected = {"a2", "b3", "c4", "e6", "f7", "g8", "a8", "b7", "c6", "e4", "f3", "g2", "h1"};
+        String[] piece1MovesActual = piece1.generateMoves();
+        System.out.println(Arrays.toString(piece1MovesActual));
+        assertArrayEquals(piece1MovesExpected, piece1MovesActual);
+        String[] piece2MovesActual = piece2.generateMoves();
+        assertArrayEquals(piece2MovesExpected, piece2MovesActual);
+    }
+}
