57d369d1ac07bd90a85d8d978a5f78f9e37cad0b
echo-977 <sr1g24@soton.ac.uk>
2025-07-14 22:29:15 +0100

    Added recursiveCaptureCheck function to see if a LinearPiece can capture a certain square (to be used in a future canCaptureKing method which will be used to find out if moves are checks), also added two helper methods to get directions for this

--- src/main/java/LinearPiece.java
+++ src/main/java/LinearPiece.java
@@ -16,6 +16,7 @@ public abstract class LinearPiece extends Piece {
     /**
      * Searches out in a given direction from the piece, checking if it is valid to move to that square.
      * Used in the move generation methods for rook, bishop and queen
+     *
      * @param board the board the piece is moving on
      * @param moves the current string of moves generated (legal moves are added to this)
      * @param movesIndex the index of the moves array
@@ -47,5 +48,77 @@ public abstract class LinearPiece extends Piece {
             }
         }
         return movesIndex;
+    }
+
+    /**
+     * Recursive function to move from a certain square towards the piece, checking that each square is empty.
+     *
+     * @param board the board we are searching on.
+     * @param file the current file of the square to check.
+     * @param rank the current rank of the square to check.
+     * @param fileDirection the direction to go towards the piece's square for the file.
+     * @param rankDirection the direction to go towards the piece's square for the rank.
+     * @return true or false value for whether the piece can capture the given square
+     */
+    public boolean recursiveCaptureCheck(Board board, char file, int rank, int fileDirection, int rankDirection) {
+        String currentSquare = file + String.valueOf(rank);
+        if (currentSquare.equals(getSquare())) { //base case, we are back to the piece's square
+            return true;
+        } else if (board.pieceSearch(currentSquare) != null) { //if we run into a piece then we cannot move to the target
+            return false;
+        } else { //recursive step
+            return recursiveCaptureCheck(board, (char) (file + fileDirection), rank + rankDirection, fileDirection,  rankDirection);
+        }
+    }
+
+    /**
+     * Used as part of subclasses canCaptureKing method to get the necessary directions for performing a recursive capture check.
+     *
+     * @param targetFile the file of the target square.
+     * @param targetRank the rank of the target square.
+     */
+    protected int[] getOrthogonalDirections(char targetFile, int targetRank) {
+        int file = getFile();
+        int rank = getRank();
+        int fileDirection, rankDirection;
+        if (targetFile == file) {
+            fileDirection = ChessDirections.NONE;
+            if (targetRank > rank) {
+                rankDirection = ChessDirections.DOWN;
+            } else {
+                rankDirection = ChessDirections.UP;
+            }
+        } else {
+            rankDirection = ChessDirections.NONE;
+            if (targetFile > file) {
+                fileDirection = ChessDirections.LEFT;
+            } else {
+                fileDirection = ChessDirections.RIGHT;
+            }
+        }
+        return new int[] {fileDirection, rankDirection};
+    }
+
+    /**
+     * Used as part of subclasses canCaptureKing method to get the necessary directions for performing a recursive capture check.
+     *
+     * @param targetFile the file of the target square.
+     * @param targetRank the rank of the target square.
+     */
+    protected int[] getDiagonalDirections(char targetFile, int targetRank) {
+        int file = getFile();
+        int rank = getRank();
+        int fileDirection, rankDirection;
+        if (targetFile > file) {
+            fileDirection = ChessDirections.LEFT;
+        } else {
+            fileDirection = ChessDirections.RIGHT;
+        }
+        if (targetRank > rank) {
+            rankDirection = ChessDirections.DOWN;
+        } else {
+            rankDirection = ChessDirections.UP;
+        }
+        return new int[] {fileDirection, rankDirection};
     }
 }
--- src/test/java/LinearPieceTest.java
+++ src/test/java/LinearPieceTest.java
@@ -12,8 +12,12 @@ class LinearPieceTest {
         piece = new LinearPiece(PieceType.QUEEN, PieceColour.WHITE, 'd', 4) {
             @Override
             public String[] generateMoves(Board board) {
-                // minimal implementation here to allow testing of concrete functions
-                return new String[8];
+                return new String[8]; //minimal implementation here to allow testing of concrete methods
+            }
+
+            @Override
+            public boolean canCaptureKing(Board board, String targetSquare) {
+                return false; //minimal implementation here to allow testing of concrete methods
             }
         };
     }
@@ -51,5 +55,53 @@ class LinearPieceTest {
         assertArrayEquals(expectedMoves, moves);
     }
 
+    @Test
+    @DisplayName("Test recursiveCaptureCheck")
+    void testRecursiveCaptureCheck() {
+        Board board = new Board("8/8/8/3q2P1/8/8/8/8 w - - 0 1");
+        LinearPiece piece = (LinearPiece) board.getBlackPieces()[0];
+        String targetSquare;
+        //targetSquare = "h5";
+        assertFalse(piece.recursiveCaptureCheck(board, 'g', 5, ChessDirections.LEFT, ChessDirections.NONE));
+        //targetSquare = "g5";
+        assertTrue(piece.recursiveCaptureCheck(board, 'f', 5, ChessDirections.LEFT, ChessDirections.NONE));
+        //targetSquare = "f5";
+        assertTrue(piece.recursiveCaptureCheck(board, 'e', 5, ChessDirections.LEFT, ChessDirections.NONE));
+    }
 
+    @Test
+    @DisplayName("Test getOrthogonalDirections")
+    void testGetOrthogonalDirections() {
+        int[] directions;
+        directions = piece.getOrthogonalDirections('d', 6);
+        assertEquals(ChessDirections.NONE, directions[ChessConstants.FILE_DIRECTION_INDEX]);
+        assertEquals(ChessDirections.DOWN, directions[ChessConstants.RANK_DIRECTION_INDEX]);
+        directions = piece.getOrthogonalDirections('g', 4);
+        assertEquals(ChessDirections.LEFT, directions[ChessConstants.FILE_DIRECTION_INDEX]);
+        assertEquals(ChessDirections.NONE, directions[ChessConstants.RANK_DIRECTION_INDEX]);
+        directions = piece.getOrthogonalDirections('d', 1);
+        assertEquals(ChessDirections.NONE, directions[ChessConstants.FILE_DIRECTION_INDEX]);
+        assertEquals(ChessDirections.UP, directions[ChessConstants.RANK_DIRECTION_INDEX]);
+        directions = piece.getOrthogonalDirections('a', 4);
+        assertEquals(ChessDirections.RIGHT, directions[ChessConstants.FILE_DIRECTION_INDEX]);
+        assertEquals(ChessDirections.NONE, directions[ChessConstants.RANK_DIRECTION_INDEX]);
+    }
+
+    @Test
+    @DisplayName("Test getDiagonalDirections")
+    void testGetDiagonalDirections() {
+        int[] directions;
+        directions = piece.getDiagonalDirections('g', 7);
+        assertEquals(ChessDirections.LEFT, directions[ChessConstants.FILE_DIRECTION_INDEX]);
+        assertEquals(ChessDirections.DOWN, directions[ChessConstants.RANK_DIRECTION_INDEX]);
+        directions = piece.getDiagonalDirections('g', 1);
+        assertEquals(ChessDirections.LEFT, directions[ChessConstants.FILE_DIRECTION_INDEX]);
+        assertEquals(ChessDirections.UP, directions[ChessConstants.RANK_DIRECTION_INDEX]);
+        directions = piece.getDiagonalDirections('a', 1);
+        assertEquals(ChessDirections.RIGHT, directions[ChessConstants.FILE_DIRECTION_INDEX]);
+        assertEquals(ChessDirections.UP, directions[ChessConstants.RANK_DIRECTION_INDEX]);
+        directions = piece.getDiagonalDirections('a', 7);
+        assertEquals(ChessDirections.RIGHT, directions[ChessConstants.FILE_DIRECTION_INDEX]);
+        assertEquals(ChessDirections.DOWN, directions[ChessConstants.RANK_DIRECTION_INDEX]);
+    }
 }
