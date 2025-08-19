e876fc04afdcac3145690440a4d25fb22e772247
echo-977 <sr1g24@soton.ac.uk>
2025-08-19 18:04:59 +0100

    Added handleDisambiguation and setDisambiguationFlags methods to deal with disambiguation in future move generation.

--- src/main/java/util/ChessConstants.java
+++ src/main/java/util/ChessConstants.java
@@ -3,6 +3,8 @@ public class ChessConstants {
 
     public static final int NUM_PIECES = 32;
     public static final int NUM_SQUARES = 64;
+    public static final int NUM_FILES = 8;
+    public static final int NUM_RANKS = 8;
     public static final int NUM_PROMOTIONS = 4;
 
     public static final int MAX_PAWN_MOVES = 12;
