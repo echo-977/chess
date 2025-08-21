8ff5b404f971db2da32d084aae62b89e0c69a78e
echo-977 <sr1g24@soton.ac.uk>
2025-08-21 21:57:47 +0100

    Moved disambiguation handling and setting functions to an external DisambiguationUtils class to declutter the board class

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -484,95 +484,6 @@ public class Board {
     }
 
     /**
-     * Detects when disambiguation is required in an array of moves.
-     * Then passes all cases of this to the setDisambiguationFlags method.
-     * @param moves the moves that need to be checked for when disambiguation is required.
-     */
-    public void handleDisambiguation(Move[] moves) {
-        int numMoves = moves.length;
-        int moveCount = 0;
-        Move[] needDisambiguation = new Move[numMoves];
-        int handledIndex = 0;
-        Move[] handled = new Move[numMoves];
-        boolean disambiguated = false;
-        boolean handledMove = false;
-        for (int i = 0; i < numMoves; i++) {
-            for (Move move : handled) {
-                if (moves[i] == move) {
-                    handledMove = true;
-                    break;
-                }
-            }
-            if (handledMove) {
-                handledMove = false;
-                continue;
-            }
-            needDisambiguation[moveCount] = moves[i]; //move shares a destination with itself
-            moveCount++;
-            handled[handledIndex] = moves[i];
-            handledIndex++;
-            for (int j = i + 1; j < numMoves; j++) {
-                if (moves[i].getDestination().equals(moves[j].getDestination()) &&
-                        moves[i].getPiece().getType() == moves[j].getPiece().getType()) {
-                    needDisambiguation[moveCount] = moves[j];
-                    moveCount++;
-                    handled[handledIndex] = moves[j];
-                    handledIndex++;
-                    disambiguated = true;
-                }
-            }
-            if (disambiguated) {
-                setDisambiguationFlags(needDisambiguation);
-                disambiguated = false;
-            }
-            needDisambiguation = new Move[numMoves];
-            moveCount = 0;
-        }
-
-    }
-
-    /**
-     * Sets the relevant disambiguation flags for a list of moves that have the same piece type and same destination.
-     * @param needDisambiguation the moves that need disambiguation.
-     */
-    public void setDisambiguationFlags(Move[] needDisambiguation) {
-        boolean needsFileDisambiguation = true; //default disambiguation method
-        boolean needsRankDisambiguation = false;
-        int[] numPiecesPerFile = new int[ChessConstants.NUM_FILES];
-        int[] numPiecesPerRank = new int[ChessConstants.NUM_RANKS];
-        for (Move move : needDisambiguation) {
-            if (move == null) {
-                continue;
-            }
-            Piece piece = move.getPiece();
-            int fileIndex = piece.mapFile();
-            int rankIndex = piece.mapRank();
-            numPiecesPerFile[fileIndex]++;
-            numPiecesPerRank[rankIndex]++;
-        }
-        for (int i = 0; i < ChessConstants.NUM_FILES; i++) {
-            if (numPiecesPerFile[i] > 1) {
-                needsRankDisambiguation = true;
-                needsFileDisambiguation = false;
-                break;
-            }
-        }
-        for (int i = 0; i < ChessConstants.NUM_RANKS; i++) {
-            if (numPiecesPerRank[i] > 1) {
-                needsFileDisambiguation = true;
-                break;
-            }
-        }
-        for (Move move : needDisambiguation) {
-            if (move == null) {
-                continue;
-            }
-            move.setFileDisambiguation(needsFileDisambiguation);
-            move.setRankDisambiguation(needsRankDisambiguation);
-        }
-    }
-
-    /**
      * Generates all the moves for a given colour.
      * @param colour the colour we want to generate moves for.
      * @return an array of all the moves the colour can do.
@@ -608,7 +519,7 @@ public class Board {
                 }
             }
         }
-        handleDisambiguation(moves);
+        DisambiguationUtils.handleDisambiguation(moves);
         return moves;
     }
 
---
+++ src/test/java/DisambiguationUtilsTest.java
@@ -0,0 +1,71 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DisambiguationUtilsTest {
+    @Test
+    @DisplayName("Test setDisambiguationFlags (file only)")
+    void testSetDisambiguationFlagsFile() {
+        Board board = new Board("8/8/3N1N2/2N5/4p3/6N1/8/8 w - - 0 1");
+        Move[] moves = {new Move(board, board.pieceSearch("c5"), "e4"),
+                new Move(board, board.pieceSearch("d6"), "e4"),
+                new Move(board, board.pieceSearch("f6"), "e4"),
+                new Move(board, board.pieceSearch("g3"), "e4")};
+        DisambiguationUtils.setDisambiguationFlags(moves);
+        for (Move move : moves) {
+            assertTrue(move.isFileDisambiguation());
+            assertFalse(move.isRankDisambiguation());
+        }
+    }
+
+    @Test
+    @DisplayName("Test setDisambiguationFlags (rank only)")
+    void testSetDisambiguationFlagsRank() {
+        Board board = new Board("8/8/8/2N5/4p3/2N5/8/8 w - - 0 1");
+        Move[] moves = {new Move(board, board.pieceSearch("c5"), "e4"),
+                new Move(board, board.pieceSearch("c3"), "e4")};
+        DisambiguationUtils.setDisambiguationFlags(moves);
+        for (Move move : moves) {
+            assertFalse(move.isFileDisambiguation());
+            assertTrue(move.isRankDisambiguation());
+        }
+    }
+
+    @Test
+    @DisplayName("Test setDisambiguationFlags (both)")
+    void testSetDisambiguationFlagsBoth() {
+        Board board = new Board("8/8/8/2N5/4p3/2N3N1/8/8 w - - 0 1");
+        Move[] moves = {new Move(board, board.pieceSearch("c5"), "e4"),
+                new Move(board, board.pieceSearch("c3"), "e4"),
+                new Move(board, board.pieceSearch("g3"), "e4")};
+        DisambiguationUtils.setDisambiguationFlags(moves);
+        for (Move move : moves) {
+            assertTrue(move.isFileDisambiguation());
+            assertTrue(move.isRankDisambiguation());
+        }
+    }
+
+    @Test
+    @DisplayName("Test handleDisambiguation")
+    void  testHandleDisambiguation() {
+        Board board = new Board("8/8/8/2N5/4p1R1/2N3N1/8/8 w - - 0 1");
+        Move[] moves = {new Move(board, board.pieceSearch("c5"), "e4"),
+                new Move(board, board.pieceSearch("c3"), "e4"),
+                new Move(board, board.pieceSearch("g3"), "e4"),
+                new Move(board, board.pieceSearch("c5"), "d7"),
+                new Move(board, board.pieceSearch("g4"), "e4")};
+        DisambiguationUtils.handleDisambiguation(moves);
+        assertTrue(moves[0].isFileDisambiguation());
+        assertTrue(moves[0].isRankDisambiguation());
+        assertTrue(moves[1].isFileDisambiguation());
+        assertTrue(moves[1].isRankDisambiguation());
+        assertTrue(moves[2].isFileDisambiguation());
+        assertTrue(moves[2].isRankDisambiguation());
+        assertFalse(moves[3].isFileDisambiguation());
+        assertFalse(moves[3].isRankDisambiguation());
+        assertFalse(moves[4].isFileDisambiguation());
+        assertFalse(moves[4].isRankDisambiguation());
+    }
+}
