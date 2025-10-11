5e6de0eac2a486c21d489610ccee979f12fa344a
echo-977 <sr1g24@soton.ac.uk>
2025-10-11 15:42:12 +0100

    Added getMobility function which returns the number of squares pieces in the position can move to.

--- src/main/java/engine/Evaluation.java
+++ src/main/java/engine/Evaluation.java
@@ -1,13 +1,16 @@
 public class Evaluation {
+    public static int MOBILITY_COEFFICIENT = 1;
     /**
      * Evaluates the position.
      * @param position the position to be evaluated.
      * @return integer representing the evaluation for the position in terms of centipawns.
      */
     public static int getEvaluation(Position position) {
+        Board board = position.getBoard();
         PieceColour colour = position.getGameState().getTurn();
         PieceColour enemyColour = colour.opponentColour();
-        int evaluation = getValueSum(position.getBoard(), colour) - getValueSum(position.getBoard(), enemyColour);
+        int evaluation = getValueSum(board, colour) - getValueSum(board, enemyColour);
+        evaluation += MOBILITY_COEFFICIENT * (getMobility(board, colour) - getMobility(board, colour));
         return evaluation;
     }
 
@@ -26,5 +29,29 @@ public class Evaluation {
             occupancy &= occupancy - 1;
         }
         return sum;
+    }
+
+    /**
+     * Calculates the number of squares the piece can actually move to.
+     * Ignores the king's squares since a kings mobility is often not a benefit for the position.
+     * Ignores the pawns squares since a pawns mobility is constant.
+     * @param board the board for the side to move on.
+     * @param colour the colour to find the mobility of.
+     * @return integer representing the total number squares the side can move to.
+     */
+    public static int getMobility(Board board, PieceColour colour) {
+        long pieces = board.getOccupancyBitboard(colour.ordinal());
+        long[] ATKFR = board.getATKFR();
+        long pieceMask = pieces;
+        int mobility = 0;
+        while (pieces != 0){
+            int square = Long.numberOfTrailingZeros(pieces);
+            PieceType pieceAtSquareType = board.pieceSearch(square).getType();
+            if (pieceAtSquareType != PieceType.KING && pieceAtSquareType != PieceType.PAWN) {
+                mobility += Long.bitCount(ATKFR[square] & ~pieceMask);
+            }
+            pieces &= pieces - 1;
+        }
+        return mobility;
     }
 }
